775df1843ec1c5bb7bc618853deb771f292f98f0
lixiang <903263494@qq.com>
2021-03-02 22:07:53 +0800

    初始化工程，配置eslint、prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['alloy', 'alloy/vue'],
+  globals: {
+    Vue: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [],
+  rules: {
+    'vue/component-tags-order': ['error', { order: [['template', 'script'], 'style'] }]
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## vue 学习工程
+
+从零开始搭建工程
+
+- 配置 eslint、prettier 编码自动格式化功能（[AlloyTeam/eslint-config-alloy](https://github.com/AlloyTeam/eslint-config-alloy/blob/master/README.zh-CN.md)）
