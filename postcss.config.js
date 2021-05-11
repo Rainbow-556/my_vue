a8ff2cab99aa1686e9f097a81930ca117eb29365
lixiang <903263494@qq.com>
2021-05-11 23:01:29 +0800

    添加postcss理解注释

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,6 @@
 /**
+ * postcss是一个css处理器，用于把css解析成AST，并且提供简单可操作的API给postcss plugin去修改AST，最后把plugin修改后的AST再转成css，
+ * postcss自己本身只做css的解析，其他的事情全部交给丰富的plugin去做。
  * postcss-preset-env类似于babel的preset-env，其实是包含了一系列的postcss插件的集合（如包含autoprefixer、CSS properties）
  * 用于把现代的css语法转换成兼容旧版浏览器能识别的语法
  * 转换规则会根据.browserlist的配置来决定要转换哪些，对于已支持的浏览器就不会转（如果browserlist配置的都是比较新的话）
